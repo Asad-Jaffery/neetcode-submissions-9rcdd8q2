aaa16595474f839120c5a86414d684c491951e0f
Asad Jaffery <asadmjaffery@gmail.com>
2026-05-23 13:45:11 -0700

    Update: anagram-groups - submission-3

--- Data Structures & Algorithms/anagram-groups/submission-3.py
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -1,21 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dict with key -> set
-        #    -> lisst of words
-
-        anagrams = [] # list of anagram sets
-        grouped_words = [] # list of list of words
-
-        for word in strs: 
-            sorted_word = sorted(word)
-            if sorted_word in anagrams:
-                index = anagrams.index(sorted_word)
-                grouped_words[index].append(word)
-            else: 
-                anagrams.append(sorted_word)
-                grouped_words.append([word])
-
-        return grouped_words
-
-
         
+        anagrams = {}
+        for s in strs:
+            sorted_string = "".join(sorted(s)) # sort the string as another string
+            if sorted_string in anagrams:
+                anagrams[sorted_string].append(s)
+            else: 
+                anagrams[sorted_string] = [s]    
+        
+        return list(anagrams.values())
