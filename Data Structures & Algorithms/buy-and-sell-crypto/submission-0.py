4216df2e96112204b72454bd33c976cb0404e969
Asad Jaffery <asadmjaffery@gmail.com>
2026-05-23 13:12:54 -0700

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,11 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0 
+        # l pointerr, r pointer
+        # profit = r - l pointers 
+        
+        
+        # we only consider new answer when there is a profit to be made
+        # increase right pointer every time 
+            # if there is NOT a profit, we set the left pointer to the right pointer? 
 
-        for i in range(len(prices)):
-            for j in range(i, len(prices)):
-                profit = max(prices[j] - prices[i], profit)
 
+        # we do this until the right pointer reaches the end of the array
+
+        profit = 0
+        l, r = 0, 0
+
+        while r <= len(prices) - 1:
+            if prices[r] > prices[l]: # there is a potential new profit
+                profit = max(profit, prices[r] - prices[l])
+            else: 
+                l = r
+            r += 1
         return profit 
 
         
