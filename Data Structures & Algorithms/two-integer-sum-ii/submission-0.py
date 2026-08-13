8deb0bac93b759bb01f073c2196c37a40d23705a
Asad Jaffery <asadmjaffery@gmail.com>
2026-08-12 21:35:33 -0700

    Update: two-integer-sum-ii - submission-0

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -1,19 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-
-        # the solution can not be past the index that is greater than the target int
         l, r = 0, len(numbers) - 1
-
-        while numbers[l] + numbers[r] != target: 
-            if numbers[l] + numbers[r] < target: 
+        while l < r: 
+            curr = numbers[l] + numbers[r]
+            if curr == target:
+                return [l + 1, r + 1]
+            elif curr < target: 
                 l += 1
-            else:
+            else: 
                 r -= 1
-
-        
-        return [l + 1, r + 1]
-            
-
-
-
-        
+          
