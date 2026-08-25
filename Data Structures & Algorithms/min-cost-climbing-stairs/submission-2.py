52f165377103970faa963c95bf2bf9233099f821
Asad Jaffery <asadmjaffery@gmail.com>
2026-08-25 12:25:17 -0700

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+    
+        total_costs = [0] * (len(cost) + 1) # dp array
+
+        for i in range(len(total_costs)): # define first two indeces as just the cost to leave that floor
+            if i < 2: 
+                total_costs[i] = cost[i]
+            elif i == len(total_costs) - 1: # last element should just be the cost to get to that floor (this is solution)
+                return min(total_costs[i - 1], total_costs[i - 2])
+            else: # every other index should be cost to get to that floor + cost to leave that floor
+                total_costs[i] = cost[i] + min(total_costs[i - 1], total_costs[i - 2]) 
+
+
+
+
+
+        
