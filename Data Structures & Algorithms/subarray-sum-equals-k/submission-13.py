f8d2b60506e83ec7da3e80c821903d190f3de0ca
Asad Jaffery <asadmjaffery@gmail.com>
2026-08-26 15:12:16 -0700

    Add: subarray-sum-equals-k - submission-13

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-13.py
@@ -0,0 +1,34 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # compute subarry and keep track of the amount of subarrays of each as we go on 
+
+        # sum[indeces we are exploring] - k = sum of subarray
+            # if that exists, then we should get all of its values and add to the res 
+
+
+
+        subarrays = {}
+        subarrays[0] = 1
+
+        res = 0
+        curr = 0
+
+        for i in range(len(nums)):
+            curr += nums[i]
+            remainder = curr - k
+
+            if remainder in subarrays: 
+                res += subarrays[remainder]
+
+            if curr not in subarrays: 
+                subarrays[curr] = 0            
+            subarrays[curr] += 1
+
+
+        return res
+
+            
+
+
+
+        
